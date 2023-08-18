0648f3d68d8d650484a6eac17f6588b238fbc5cc
kalapathar <deshah2@duosecurity.com>
2023-08-17 23:38:32 -0500

    add a reflection blog post

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -66,6 +66,9 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
+              <Link className="navbar-item" to="/travel">
+                Travel
+              </Link>
               {/* <Link className="navbar-item" to="/products">
                 Products
               </Link>
